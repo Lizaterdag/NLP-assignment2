c18a40446cb900875ee757e5e4094243d0072038
lizaterdag <liza.darwesh@hva.nl>
2024-04-22 00:28:33 +0200

    removed chars

--- problem0.py
+++ problem0.py
@@ -6,9 +6,8 @@ import string
 brown_corpus = brown.words()
 word_count = Counter(brown_corpus)
 word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
-
-punc = set(string.punctuation + "``" + "''" + "..." + "''" + "``" + "!" + "?" + "." + "," + ":" + ";" + "-")
-unique_words = [(word, freq) for word, freq in word_count if word not in punc]
+bad_chars = ["``" , "''" , "..." , "''" , "``" , "!" , "?" , "." , "," , ":" , ";" , "-"]
+unique_words = [(word, freq) for word, freq in word_count if word not in bad_chars]
 
 print("Unique words:")
 for word, freq in unique_words[:20]:  
