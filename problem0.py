a5cb7a84df770c7700c1b74df6d5e23fb549ab48
lizaterdag <liza.darwesh@hva.nl>
2024-04-22 00:52:09 +0200

    all word counts and two genres

--- problem0.py
+++ problem0.py
@@ -4,11 +4,28 @@ from collections import Counter
 import string
 
 brown_corpus = brown.words()
-word_count = Counter(brown_corpus)
-word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
-bad_chars = ["``" , "''" , "..." , "''" , "``" , "!" , "?" , "." , "," , ":" , ";" , "-"]
-unique_words = [(word, freq) for word, freq in word_count if word not in bad_chars]
 
+bad_chars = ["``" , "''" , "..." , "''" , "``" , "!" , "?" , "." , "," , ":" , ";" , "-"]
+
+def get_word_freq(corpus):
+    count = Counter(corpus)
+    words = sorted(count.items(), key=lambda x: x[1], reverse=True)
+    return [(word, freq) for word, freq in words if word.strip() and word not in bad_chars]
+
+
+unique_words = get_word_freq(brown_corpus) 
 print("Unique words:")
 for word, freq in unique_words[:20]:  
+    print(word, freq)
+
+romance = get_word_freq(brown.words(categories='romance'))
+humor = get_word_freq(brown.words(categories='humor'))
+
+
+print("\ngenre romance :")
+for word, freq in romance[:20]:  
+    print(word, freq)
+
+print("\ngenre humor :")
+for word, freq in humor[:20]:  
     print(word, freq)
