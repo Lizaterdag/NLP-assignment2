6e702e0181ecb612bdb3a078bfbaecb59d83faad
VKorpelshoek <v.korpelshoek@hotmail.com>
2024-04-24 15:51:17 +0200

    problem2 fix

--- problem2.py
+++ problem2.py
@@ -10,33 +10,29 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 import numpy as np
 from generate import GENERATE
-
+import codecs
 
 vocab = open("brown_vocab_100.txt")
-
 #load the indices dictionary
 word_index_dict = {}
 for i, line in enumerate(vocab):
-    #TODO: import part 1 code to build dictionary
     word_index_dict[line.rstrip()] = i
-    
+
 f = open("brown_100.txt")
 
-#TODO: initialize counts to a zero vector
 counts = np.zeros(len(word_index_dict))
-#TODO: iterate through file and update counts
 
 for line in f:
     for word in line.split():
         word.lower()
         if word in word_index_dict:
             counts[word_index_dict[word]] += 1
-f.close()
-print(counts)
 
-#TODO: normalize and writeout counts. 
+f.close()
 probs = counts / np.sum(counts)
-np.save("unigram_probs.txt", probs)
-print(probs)
+
+np.savetxt('unigram_probs.txt', probs)
+
+
 
 
